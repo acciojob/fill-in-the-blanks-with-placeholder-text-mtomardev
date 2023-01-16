96db90fa60c20bdd17ee74a9647cde2ff5818cf1
mtomardev <120013813+mtomardev@users.noreply.github.com>
2023-01-17 04:08:56 +0530

    second code

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
 //your code here
+let element = document.getElementById('p1');
+element.innerHTML = "Accio ipsum dolor sit amet, join course, solve assignments, get placed above 5 lpa and live life king size.";
